04521a91d471e6d96810a1f71dd69ab28ba47f69
Chihiro Kimura <chittaro10@gmail.com>
2025-02-25 13:18:52 +0900

    chore: 古いコンポーネントファイルを削除

--- components/ErrorState.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorState() {
-  return (
-    <div className="flex items-center justify-center h-[80vh] text-rose-400">
-      <p>エラーが発生しました。再度お試しください。</p>
-    </div>
-  );
-}
